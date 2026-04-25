ef6617f22180bbe7e43e8751bdb13d5a5f1aab0c
filipefilardi <filipefilardi@gmail.com>
2026-04-24 23:41:21 -0300

    Refactor project namespace and identifiers to "com.silent_summit.jellymusic". Update Android and iOS configurations, including compileSdk and applicationId. Introduce MainActivity for audio service integration. Clean up router and mini player code for improved readability.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
-    namespace = "com.jellymusic.jellymusic"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.silent_summit.jellymusic"
+    // Align with plugin requirements (flutter_secure_storage, audio_*, etc.)
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,10 +22,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.jellymusic.jellymusic"
+        applicationId = "com.silent_summit.jellymusic"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // 24+ required by flutter_secure_storage
+        minSdk = maxOf(24, flutter.minSdkVersion)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
